4679c1930c660dca062ed97683706078313044a1
arch-err <archer@jesber.xyz>
2024-11-24 15:21:57 +0100

    feat: delete-button

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"html/template"
 	"io"
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct {
@@ -21,19 +24,28 @@ func newTemplate() *Templates {
 	}
 }
 
+var id int = 0
+
 type Contact struct {
 	Name  string
 	Email string
+	Id    int
 }
 
 func newContact(name, email string) Contact {
+	id++
 	return Contact{
 		Name:  name,
 		Email: email,
+		Id:    id,
 	}
 }
 
 type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
 
 func (d Data) hasEmail(email string) bool {
 	for _, contact := range d.Contacts {
@@ -44,8 +56,13 @@ func (d Data) hasEmail(email string) bool {
 	return false
 }
 
-type Data struct {
-	Contacts Contacts
+func (d Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+	return -1
 }
 
 func newData() Data {
@@ -87,6 +104,8 @@ func main() {
 
 	e := echo.New()
 	e.Use(middleware.Logger())
+	e.Static("/images", "images")
+	e.Static("/css", "css")
 
 	page := newPage()
 	e.Renderer = newTemplate()
@@ -114,5 +133,23 @@ func main() {
 		c.Render(200, "form", newFormData())
 		return c.Render(200, "oob-contact", contact)
 	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		time.Sleep(3 * time.Second)
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(400, "Invalid id")
+		}
+
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(404, "Contact not found")
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+		return c.NoContent(200)
+	})
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
